d51b0f364f5a0abafe850dd989adc5be5c2bfd61
Simu_Sohil_R <151726410+SimuSohil@users.noreply.github.com>
2024-08-07 11:51:54 +0530

    creating backend and frontend

---
+++ frontend/build/phone_state_background/generated/source/buildConfig/release/me/sodipto/phone_state_background/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.sodipto.phone_state_background;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "me.sodipto.phone_state_background";
+  public static final String BUILD_TYPE = "release";
+}
